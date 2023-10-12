8883d6bec7dd780d43d01cedf9ded3430035c5ac
joshwhite2 <126173462+joshwhite2@users.noreply.github.com>
2023-10-12 10:02:37 -0230

    reads html

--- http.js
+++ http.js
@@ -1,61 +1,49 @@
-const http = require('http');
 
+
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-    // Use a switch statement to handle different routes
+    let filePath = "./views/";
+
     switch (url) {
         case '/about':
-            console.log('About page');
-            res.write("About Page");
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end('About Page');
+            filePath += "about.html";
             break;
         case '/contact':
-            console.log('Contact page');
-            res.write("Contact Page");
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end('Contact Page');
+            filePath += "contact.html";
             break;
         case '/products':
-            console.log('Products page');
-            res.write("Products Page");
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end('Products Page');
+            filePath += "products.html";
             break;
         case '/subscribe':
-            console.log('Subscribe page');
-            res.write("Subscribe Page");
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end('Subscribe Page');
+            filePath += "subscribe.html";
             break;
         default:
-            res.write("Home Page");
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end('Home Page');
+            filePath += "index.html";
             break;
-    }})
-;
+    }
 
-// const server = http.createServer((req, res) => {
-//     if (req.url === '/') {
-//         res.write('Hello World');
-        
-//         res.end();
-//     }else if(req.url === '/about'){
-//         res.write(
-//             'About page yeyuh' );
-//         res.end();
-//     }else if(req.url === '/contact'){
-//         res.write(
-//             'Contact page');
-//         res.end();
-//     }}
-// )
+    // Read the HTML file and serve it as a response
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.error(err);
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not Found');
+        } else {
+            console.log(`${url} page`);
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
+});
 
 server.on('connection', () => {
     console.log('A new connection has been established');
 });
 server.listen(3000);
 
-console.log('listening on 3000');
+console.log('Listening on http://localhost:3000');
